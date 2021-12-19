f6f0ba35b47f3d3fee1dc4ed440fb328b20d5eb6
Madhav Jadhav <86906584+jadhavmadhav@users.noreply.github.com>
2021-12-19 22:36:03 +0530

    Add files via upload
    Add Books  Image in Card

--- books.js
+++ books.js
@@ -1,17 +1,86 @@
- 
+  
   
  
 const Show_Books =()=>{
    
-    setTimeout(async()=>{
+  setTimeout(async()=>{
+        const req = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${userdata}:keyes&AIzaSyAx77nEfTm5MEbI9SfOtxU_P7TxgHThiFY`)
+        const result = await req.json()
+
+        const Data = result.items
+
+     Data.map((Data)=>{
+            // console.log(Data.volumeInfo)
+           const bookName=Data.volumeInfo.imageLinks.thumbnail
+           const publisher=Data.volumeInfo.publisher
+           const pages =Data.volumeInfo.pageCount
+           const rating =Data.volumeInfo.averageRating
+           const Country = Data.saleInfo.country
+           const Ebook =Data.saleInfo.isEbook 
+          
+          //  Show data in Our UI 
+          const Display =document.getElementById('demo')  //Target that element and return the cards
+
+           return Display.innerHTML+=`
+           <div class="col-12 col-md-4 col-lg-3 p-4">
+           <div class="card">
+             <div class="card-header"> 
+              <div class="card-img-top">
+              <img src=${bookName} alt="Card image cap"> 
+              </div>
+                <div class="row"> 
+               <p class='col-6 col-md-6'> pages: ${pages}</p>
+               </div>
+             </div>
+             <div class="card-body">
+               <div class="row">
+                 <div class="col-6 col-md-6">
+                   <h6>country:  ${Country}</h6>
+                   <h6>Rating:<span id='rating'> ${ rating}</span></h6> 
+                 </div>
+                 <div class="col-6 col-md-6">
+                   <h6>Ebook: <span id='ebook'> ${Ebook} </span></h6>
+                    
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+           `
+
+        })
+     
+  })
+
+}
+Show_Books()
+
+
+// ---------------------------------------------------------------------------
+
+const Search =()=>{
+  const userdata =document.getElementById('userdata').value
+   
+  setTimeout(async()=>{
+     
+      try {
+          
           const req = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${userdata}:keyes&AIzaSyAx77nEfTm5MEbI9SfOtxU_P7TxgHThiFY`)
+           
+     if (!req.ok) {
+         throw Error.req(status)
+     }
+     else{
+      document.getElementById('demo').style.display="none"
+      // User Field Empty 
+      document.getElementById('userdata').value=""
+       
           const result = await req.json()
-
-          const Data = result.items
-
+         const Data =result.items
+  
        Data.map((Data)=>{
             //   console.log(Data.volumeInfo.publisher)
-             const bookName=Data.volumeInfo.title
+             const bookName=Data.volumeInfo.imageLinks.thumbnail
              const publisher=Data.volumeInfo.publisher
              const pages =Data.volumeInfo.pageCount
              const rating =Data.volumeInfo.averageRating
@@ -19,15 +88,16 @@ const Show_Books =()=>{
              const Ebook =Data.saleInfo.isEbook 
             
             //  Show data in Our UI 
-            const Display =document.getElementById('demo')  //Target that element and return the cards
-
+            const Display =document.getElementById('showdata')  //Target that element and return the cards
+  
              return Display.innerHTML+=`
              <div class="col-12 col-md-4 col-lg-3 p-4">
              <div class="card">
-               <div class="card-header">
-                 <h5>${bookName}</h5>
-                 <div class="row">
-                 <p class='col-6 col-md-6'>-${publisher}</p>
+               <div class="card-header"> 
+                <div class="card-img-top">
+                <img src=${bookName} alt="Card image cap"> 
+                </div>
+                  <div class="row"> 
                  <p class='col-6 col-md-6'> pages: ${pages}</p>
                  </div>
                </div>
@@ -46,80 +116,12 @@ const Show_Books =()=>{
              </div>
            </div>
              `
-
+  
           })
-       
-    })
-
-}
-Show_Books()
-
-
-// ---------------------------------------------------------------------------
-
-const Search =()=>{
-    const userdata =document.getElementById('userdata').value
-     
-    setTimeout(async()=>{
-       
-        try {
-            
-            const req = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${userdata}:keyes&AIzaSyAx77nEfTm5MEbI9SfOtxU_P7TxgHThiFY`)
-             
-       if (!req.ok) {
-           throw Error.req(status)
-       }
-       else{
-        document.getElementById('demo').style.display="none"
-        // User Field Empty 
-        document.getElementById('userdata').value=""
-         
-            const result = await req.json()
-           const Data =result.items
+      }
+  } catch (error) {
+          console.log(error)
+      }
+})
     
-         Data.map((Data)=>{
-              //   console.log(Data.volumeInfo.publisher)
-               const bookName=Data.volumeInfo.title
-               const publisher=Data.volumeInfo.publisher
-               const pages =Data.volumeInfo.pageCount
-               const rating =Data.volumeInfo.averageRating
-               const Country = Data.saleInfo.country
-               const Ebook =Data.saleInfo.isEbook 
-              
-              //  Show data in Our UI 
-              const Display =document.getElementById('showdata')  //Target that element and return the cards
-    
-               return Display.innerHTML+=`
-               <div class="col-md-3 p-4">
-               <div class="card ">
-                 <div class="card-header">
-                   <h5>${bookName}</h5>
-                   <div class="row">
-                   <p class='col-md-6'>-${publisher}</p>
-                   <p class='col-md-6'> pages: ${pages}</p>
-                   </div>
-                 </div>
-                 <div class="card-body">
-                   <div class="row">
-                     <div class="col-md-6">
-                       <h6>country:  ${Country}</h6>
-                       <h6>Rating:<span id='rating'> ${ rating}</span></h6> 
-                     </div>
-                     <div class="col-md-6">
-                       <h6>Ebook: <span id='ebook'> ${Ebook} </span></h6>
-                        
-                     </div>
-                   </div>
-                 </div>
-               </div>
-             </div>
-               `
-    
-            })
-        }
-    } catch (error) {
-            console.log(error)
-        }
-  })
-      
 }
